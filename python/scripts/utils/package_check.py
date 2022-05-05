14058f194235f174d00268507ed79d31cdd2e947
xeptao <xeptaowastaken@gmail.com>
2022-05-05 20:02:13 +0300

    refactor(py): throw an error instead of printing

--- python/scripts/utils/package_check.py
+++ python/scripts/utils/package_check.py
@@ -1,11 +1,5 @@
-#  1 - passed
-#  0 - failed
 def package_check(package_name, error_message):
     try:
         __import__(package_name)
-
-        return 1
     except ImportError:
-        print(error_message)
-
-        return 0
+        raise error_message

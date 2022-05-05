1ff6c9349f7608c36ab7ea02de4cb982c845f3ac
xeptao <xeptaowastaken@gmail.com>
2022-05-05 18:19:06 +0300

    refactor: implement pytest

--- python/src/factorial/factorial.test.py
+++
@@ -1,8 +0,0 @@
-from factorial import factorial
-
-result = factorial(6)
-
-if result == 720:
-    print("\033[92mfactorial: passed\033[0m")
-else:
-    raise Exception("\033[91mfactorial - failed\033[91m")
---
+++ python/src/factorial/test_factorial.py
@@ -0,0 +1,5 @@
+from factorial import factorial
+
+
+def test_answer():
+    assert factorial(3) == 6
